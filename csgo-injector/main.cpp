9ccbcea5546f43358cb488e32324d3333567cd51
wmbtbc2 <wmbtbc2@yahoo.com>
2017-08-12 21:50:44 -0700

    Add files via upload

---
+++ csgo-injector/main.cpp
@@ -0,0 +1,26 @@
+#include <Windows.h>
+#include <stdio.h>
+#include "Mem.h"
+typedef LRESULT(WINAPI * MyProc)(int code, WPARAM wp, LPARAM lp); // export from calc_dll.dll
+
+int main()
+{
+	auto pStartupInfo = new STARTUPINFO();
+	auto pProcessInfo = new PROCESS_INFORMATION();
+
+	CreateProcess(L"C:/Program Files (x86)/Steam/steamapps/common/Counter-Strike Global Offensive/csgo.exe", L" -secure -safe -steam -novid -tickrate 128", nullptr, nullptr, FALSE, NORMAL_PRIORITY_CLASS, nullptr, nullptr, pStartupInfo, pProcessInfo);
+	Sleep(15000);
+
+	HMODULE dll = LoadLibrary(L"module.dll");
+	HOOKPROC addr = (HOOKPROC)GetProcAddress(dll, "Test");
+	HHOOK ass = SetWindowsHookEx(WH_KEYBOARD, addr, dll, pProcessInfo->dwThreadId);
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+
+	}
+	UnhookWindowsHookEx(ass);
+	FreeConsole();
+	exit(0);
+}
+
